142433d1cd471a71c575d45d3e37897effb229df
corner <corner3499@gmail.com>
2023-02-26 21:00:51 +0900

    initial

---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+function Header() {
+	return <div>Delete This</div>;
+}
+
+export default Header;
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+function Main() {
+	return (
+		<div>
+			<div>Delete This</div>
+		</div>
+	);
+}
+
+export default Main;
